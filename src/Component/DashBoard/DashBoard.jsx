6a29c9b6cef2bcaba5e2c4760edcb057094126e8
FaisalHasanEmon <faisalhasan5833@gmail.com>
2024-11-04 22:58:13 +0600

    navbar and footer added

---
+++ src/Component/DashBoard/DashBoard.jsx
@@ -0,0 +1,9 @@
+const DashBoard = () => {
+  return (
+    <div>
+      <h2 className="text-5xl">Dishum dishum dashing board</h2>
+    </div>
+  );
+};
+
+export default DashBoard;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Component/Root/Root";
+import ErrorPage from "./Component/ErrorPage/ErrorPage";
+import Home from "./Component/Home/Home";
+import DashBoard from "./Component/DashBoard/DashBoard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/dashboard",
+        element: <DashBoard></DashBoard>,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
